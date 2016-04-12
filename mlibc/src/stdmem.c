764f6d4d9d5fda15dc93125ff45fc185e5830cbd
Ross Gardiner <ross-public@gmx.com>
2016-04-12 14:07:54 +0100

    Implement simple versions of stdmem_allocate, stdmem_copy and stdmem_free

--- mlibc/src/stdmem.c
+++ mlibc/src/stdmem.c
@@ -1,12 +1,26 @@
 #include <stdmem.h>
 #include <stddef.h>
+#include <syscall.h>
 
 void* stdmem_allocate(size_t size) {
-        return NULL;
+        // A very simple memory allocator
+        void *p = _sbrk(0);
+        if (_sbrk(size) == (void*)-1) {
+                return NULL;
+        } else {
+                return p;
+        }
 }
 
 void* stdmem_copy(void* destination, const void* source, size_t numbytes) {
-        return NULL;
+        char* src = (char*)src;
+        char* dest = (char*)dest;
+
+        for (int i = 0 ; i < numbytes ; i++) {
+                destination[i] = source[i];
+        }
+
+        return destination;
 }
 
 void stdmem_free(void* ptr) {
--- os/src/user/syscall.c
+++ os/src/user/syscall.c
@@ -1,78 +1,70 @@
 #include "syscall.h"
 
 void _yield() {
-  asm volatile( "svc #158     \n"  );
+        asm volatile("svc #158\n");
 }
-
-/* Initial implementations of these syscalls adapted from https://balau82.wordpress.com/2010/12/16/using-newlib-in-arm-bare-metal-programs/ */
 
 void _exit(int status) {
-        asm volatile( "mov r0, %0 \n"
-                      "svc #0     \n"
-                    :
-                    : "r" (status)
-                    : "r0" );
+        asm volatile(
+                "mov r0, %0 \n"
+                "svc #0     \n"
+                :
+                : "r" (status)
+                : "r0");
 }
 
-int _close(int file) { return -1; }
-
-int _fstat(int file, struct stat *st) {
- st->st_mode = S_IFCHR;
- return 0;
-}
-
-int _isatty(int file) { return 1; }
-
-int _lseek(int file, int ptr, int dir) { return 0; }
-
-int _open(const char *name, int flags, int mode) { return -1; }
-
-int _read(int fd, char *buf, size_t nbytes) {
-  int r;
-
-  asm volatile( "mov r0, %1 \n"
+int32_t _read(int32_t fd, char *buf, size_t nbytes) {
+        int r;
+        asm volatile(
+                "mov r0, %1 \n"
                 "mov r1, %2 \n"
                 "mov r2, %3 \n"
                 "svc #3     \n"
                 "mov %0, r0 \n"
-              : "=r" (r)
-              : "r" (fd), "r" (buf), "r" (nbytes)
-              : "r0", "r1", "r2" );
-
-  return r;
+                : "=r" (r)
+                : "r" (fd), "r" (buf), "r" (nbytes)
+                : "r0", "r1", "r2");
+        return r;
 }
 
-char *heap_end = 0;
-caddr_t _sbrk(int incr) {
-        extern char heap_low; /* Defined by the linker */
-        extern char heap_top; /* Defined by the linker */
-        char *prev_heap_end;
+char* current_break = 0;
+void* _sbrk(int32_t incr) {
+        extern char heap_low; // Defined by the linker
+        extern char heap_top; // Defined by the linker
+        char *prev_break;
 
-        if (heap_end == 0) {
-                heap_end = &heap_low;
-        }
-        prev_heap_end = heap_end;
-
-        if (heap_end + incr > &heap_top) {
-                /* Heap and stack collision */
-                return (caddr_t)0;
+        // If this is the first time, set the current break
+        // location to the start of the unmapped region
+        if (current_break == 0) {
+                current_break = &heap_low;
         }
 
-        heap_end += incr;
-        return (caddr_t) prev_heap_end;
+        // Record the location of the current break before
+        // we map more of the unallocated space
+        prev_break = current_break;
+
+        // If we cannot move the break by the increment without
+        // exceeding the available space, return (void*)-1
+        if (current_break + incr > &heap_top) {
+                return (void*)-1;
+        }
+
+        // Else move the break forward and return the position
+        // at the start of the increment
+        current_break += incr;
+        return (void*)prev_break;
  }
 
-ssize_t _write(int fd, char *buf, size_t nbytes) {
-  int r;
-
-  asm volatile( "mov r0, %1 \n"
+size_t _write(int32_t fd, char *buf, size_t nbytes) {
+        int r;
+        asm volatile(
+                "mov r0, %1 \n"
                 "mov r1, %2 \n"
                 "mov r2, %3 \n"
                 "svc #4     \n"
                 "mov %0, r0 \n"
-              : "=r" (r)
-              : "r" (fd), "r" (buf), "r" (nbytes)
-              : "r0", "r1", "r2" );
-
-  return r;
+                : "=r" (r)
+                : "r" (fd), "r" (buf), "r" (nbytes)
+                : "r0", "r1", "r2");
+        return r;
 }
